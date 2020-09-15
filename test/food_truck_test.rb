536f658912807fbfcbd7b98d7bb5a92beb215c54
Carson Jardine <carsonjardine@gmail.com>
2020-09-15 13:15:44 -0600

    Write further tests for checking stock

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -24,27 +24,24 @@ class FoodTruckTest < Minitest::Test
 
   def test_it_can_check_stock
     assert_equal 0, @food_truck.check_stock(@item1)
+
+    @food_truck.stock(@item1, 30)
+    assert_equal ({@item1 => 30}), @food_truck.inventory
+    assert_equal 30, @food_truck.check_stock(@item1)
   end
 
 end
 
-# @food_truck.inventory
-#=> {}
-
-# @food_truck.check_stock(item1)
-#=> 0
-
-# @food_truck.stock(item1, 30)
 
 # @food_truck.inventory
 #=> {#<Item:0x007f9c56740d48...> => 30}
 
-# @food_truck.check_stock(item1)
+# @food_truck.check_stock(@item1)
 #=> 30
 
-# @food_truck.stock(item1, 25)
+# @food_truck.stock(@item1, 25)
 
-# @food_truck.check_stock(item1)
+# @food_truck.check_stock(@item1)
 #=> 55
 
 # @food_truck.stock(item2, 12)
