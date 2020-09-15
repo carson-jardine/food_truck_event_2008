e389f9132f9fee33e7b3f96901df2efa882b6bb9
Carson Jardine <carsonjardine@gmail.com>
2020-09-15 13:30:03 -0600

    Add code to pass name test

--- lib/event.rb
+++ lib/event.rb
@@ -7,6 +7,12 @@ class Event
   end
 
   def add_food_truck(truck)
-    @food_trucks << truck 
+    @food_trucks << truck
+  end
+
+  def food_truck_names
+    @food_trucks.map do |food_truck|
+      food_truck.name
+    end 
   end
 end
