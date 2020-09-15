b5c44e8c18e2a1810ac1852acc56f4ac1f302436
Carson Jardine <carsonjardine@gmail.com>
2020-09-15 14:46:01 -0600

    add test for date

--- test/event_test.rb
+++ test/event_test.rb
@@ -1,8 +1,10 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'date'
 require './lib/item'
 require './lib/food_truck'
 require './lib/event'
+require 'mocha/minitest'
 
 class EventTest < Minitest::Test
   def setup
@@ -16,6 +18,8 @@ class EventTest < Minitest::Test
     @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
     @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
     @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    @item5 = Item.new({name: 'Onion Pie', price: '$25.00'})
+
   end
 
   def test_it_exists
@@ -143,6 +147,61 @@ class EventTest < Minitest::Test
     @event.add_food_truck(@food_truck3)
 
     expected = ["Apple Pie (Slice)", "Banana Nice Cream", "Peach Pie (Slice)", "Peach-Raspberry Nice Cream"]
-    assert_equal expected, @event.sorted_item_list 
+    assert_equal expected, @event.sorted_item_list
   end
+
+  def test_it_can_list_date
+    assert_equal "15/09/2020", @event.date.strftime("%d/%m/%Y")
+
+    @event.stubs(:date).returns("24/02/2020")
+    assert_equal "24/02/2020", @event.date("%d/%m/%Y")
+  end
+
+
 end
+
+# @food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+#=> #<FoodTruck:0x00007fe1348a1160...>
+
+# @food_truck1.stock(item1, 35)
+
+# @food_truck1.stock(item2, 7)
+
+# @food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+#=> #<FoodTruck:0x00007fe1349bed40...>
+
+# @food_truck2.stock(item4, 50)
+
+# @food_truck2.stock(item3, 25)
+
+# @food_truck3 = FoodTruck.new("Palisade Peach Shack")
+#=> #<FoodTruck:0x00007fe134910650...>
+
+# @food_truck3.stock(item1, 65)
+
+# @event.add_food_truck(food_truck1)
+
+# @event.add_food_truck(food_truck2)
+
+# @event.add_food_truck(food_truck3)
+
+# @event.sell(item1, 200)
+#=> false
+
+# @event.sell(item5, 1)
+#=> false
+
+# @event.sell(item4, 5)
+#=> true
+
+# @food_truck2.check_stock(item4)
+#=> 45
+
+# @event.sell(item1, 40)
+#=> true
+
+# @food_truck1.check_stock(item1)
+#=> 0
+
+# @food_truck3.check_stock(item1)
+#=> 60
