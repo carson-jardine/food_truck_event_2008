f4ed37a0526631a68761c676b8212879060090a4
Carson Jardine <carsonjardine@gmail.com>
2020-09-15 13:27:28 -0600

    Write tests for adding trucks

--- test/event_test.rb
+++ test/event_test.rb
@@ -7,6 +7,15 @@ require './lib/event'
 class EventTest < Minitest::Test
   def setup
     @event = Event.new("South Pearl Street Farmers Market")
+
+    @food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    @food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    @food_truck3 = FoodTruck.new("Palisade Peach Shack")
+
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
   end
 
   def test_it_exists
@@ -16,45 +25,32 @@ class EventTest < Minitest::Test
   def test_it_has_a_name
     assert_equal "South Pearl Street Farmers Market", @event.name
   end
+
+  def test_food_trucks_starts_empty
+    assert_equal [], @event.food_trucks
+  end
+
+  def test_it_can_add_food_trucks
+    @event.add_food_truck(@food_truck1)
+
+    assert_equal [@food_truck1], @event.food_trucks
+
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+    assert_equal [@food_truck1, @food_truck2, @food_truck3], @event.food_trucks
+  end
 end
 
+# @food_truck1.stock(@item1, 35)
+# @food_truck1.stock(@item2, 7)
+# @food_truck2.stock(@item4, 50)
+# @food_truck2.stock(@item3, 25)
+# @food_truck3.stock(@item1, 65)
 
-# @food_truck1 = FoodTruck.new("Rocky Mountain Pies")
-#=> #<FoodTruck:0x00007fe1348a1160...>
 
-# @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
-#=> #<Item:0x007f9c56740d48...>
-
-# @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
-#=> #<Item:0x007f9c565c0ce8...>
-
-# @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
-#=> #<Item:0x007f9c562a5f18...>
-
-# @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
-#=> #<Item:0x007f9c56343038...>
-
-# @food_truck1.stock(item1, 35)
-
-# @food_truck1.stock(item2, 7)
-
-# @food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
-#=> #<FoodTruck:0x00007fe1349bed40...>
-
-# @food_truck2.stock(item4, 50)
-
-# @food_truck2.stock(item3, 25)
-
-# @food_truck3 = FoodTruck.new("Palisade Peach Shack")
 #=> #<FoodTruck:0x00007fe134910650...>
 
-# @food_truck3.stock(item1, 65)
 
-# @event.add_food_truck(food_truck1)
-
-# @event.add_food_truck(food_truck2)
-
-# @event.add_food_truck(food_truck3)
 
 # @event.food_trucks
 #=> [#<FoodTruck:0x00007fe1348a1160...>, #<FoodTruck:0x00007fe1349bed40...>, #<FoodTruck:0x00007fe134910650...>]
@@ -62,10 +58,10 @@ end
 # @event.food_truck_names
 #=> ["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
 
-# @event.food_trucks_that_sell(item1)
+# @event.food_trucks_that_sell(@item1)
 #=> [#<FoodTruck:0x00007fe1348a1160...>, #<FoodTruck:0x00007fe134910650...>]
 
-# @event.food_trucks_that_sell(item4)
+# @event.food_trucks_that_sell(@item4)
 #=> [#<FoodTruck:0x00007fe1349bed40...>]
 
 # @food_truck1.potential_revenue
