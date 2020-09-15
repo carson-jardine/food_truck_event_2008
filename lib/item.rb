2530c3ee3e7a26db62649fd1a4fd2ba417b6ccff
Carson Jardine <carsonjardine@gmail.com>
2020-09-15 13:06:59 -0600

    Write attributes test and code to pass

--- lib/item.rb
+++ lib/item.rb
@@ -1,4 +1,11 @@
 class Item
+  attr_reader :name
   def initialize(attributes)
+    @name = attributes[:name]
+    @price = attributes[:price]
+  end
+
+  def price
+    @price.delete('$').to_f
   end
 end
