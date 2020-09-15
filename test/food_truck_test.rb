95f781f51f162c5d78aa41464503d75c18025f09
Carson Jardine <carsonjardine@gmail.com>
2020-09-15 13:22:16 -0600

    Setup event tests

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -31,18 +31,8 @@ class FoodTruckTest < Minitest::Test
 
     @food_truck.stock(@item1, 25)
     assert_equal 55, @food_truck.check_stock(@item1)
+
+    @food_truck.stock(@item2, 12)
+    assert_equal ({@item1 => 55, @item2 => 12}), @food_truck.inventory
   end
-
 end
-
-# assert_equal ({@item1 => 30}), @food_truck.inventory
-
-
-
-# @food_truck.check_stock(@item1)
-#=> 55
-
-# @food_truck.stock(item2, 12)
-
-# @food_truck.inventory
-#=> {#<Item:0x007f9c56740d48...> => 55, #<Item:0x007f9c565c0ce8...> => 12}
